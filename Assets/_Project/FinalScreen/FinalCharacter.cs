dd84ba9b21bc6c26841534078cee1cc99fbd2b59
Applesinus blueback <dmitryapelsin4ik@gmail.com>
2026-08-18 23:40:23 +0300

    add final screen starter

---
+++ Assets/_Project/FinalScreen/FinalCharacter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalCharacter : MonoBehaviour
+{
+    [SerializeField] public Text CharacterName;
+    [SerializeField] public Image CharacterImage;
+    [SerializeField] public Sprite GoodFinal;
+    [SerializeField] public Sprite BadFinal;
+
+    public void SetFinal()
+    {
+        
+    }
+}
